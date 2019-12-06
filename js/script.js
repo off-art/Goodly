896e15a4dede32cfc98c15de59faea8408ac5fe9
off-art <troy750@yandex.ru>
2019-12-06 23:49:37 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 let desc = document.querySelectorAll('.item-card__desc_text'); // Кнопка "Важная информация"
 let hide = document.querySelectorAll('.item-card-hide'); // Скрытый блок
 let close = document.querySelectorAll('.item-card-hide_close'); // Кнопка крестик
+
+
 // Кнопка переключатель
 desc.forEach(function(element) {
   element.onclick = function(e) {
     e.preventDefault();
+    // e.currentTarget.parentNode += 'snow';
     hide.forEach(function(elem) {
       elem.classList.toggle('show')
     })
